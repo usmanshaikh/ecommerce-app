85bbccf4887e3e4afb1be0bf9f496d263c3799db
Usman Shaikh <shaikhusman57@gmail.com>
2025-06-26 00:09:35 +0530

    Implement login and registration forms with validation using Formik and Yup; add message constants for validation feedback

--- frontend/src/pages/Auth/Login.tsx
+++ frontend/src/pages/Auth/Login.tsx
@@ -1,7 +1,91 @@
-import React from 'react';
+import * as yup from 'yup';
+import { useFormik } from 'formik';
+import { useNavigate } from 'react-router-dom';
+import { Box, Button, TextField, Typography } from '@mui/material';
+import { MSG, ROUTES } from '../../utils/constants';
+import './Auth.scss';
+
+const validationSchema = yup.object({
+  email: yup.string().email(MSG.VALIDATION.EMAIL.INVALID).required(MSG.VALIDATION.EMAIL.REQUIRED),
+  password: yup
+    .string()
+    .min(MSG.VALIDATION.PASSWORD.MIN_LENGTH, MSG.VALIDATION.PASSWORD.LENGTH_ERROR)
+    .required(MSG.VALIDATION.PASSWORD.REQUIRED),
+});
 
 const Login = () => {
-  return <div>Login</div>;
+  const formik = useFormik({
+    initialValues: {
+      email: '',
+      password: '',
+    },
+    validationSchema: validationSchema,
+    onSubmit: (values) => {
+      handleLogin();
+    },
+  });
+
+  const navigate = useNavigate();
+
+  const handleLogin = () => {};
+
+  const handleRegister = () => {};
+
+  return (
+    <Box className="login-page">
+      <Box sx={{ width: 400 }}>
+        <Typography variant="h4" fontWeight={600} gutterBottom>
+          Login
+        </Typography>
+        <Typography variant="subtitle1" color="text.secondary">
+          Hello Again! Welcome back
+        </Typography>
+        <Box>
+          <form onSubmit={formik.handleSubmit}>
+            <Box sx={{ my: 2 }}>
+              <TextField
+                fullWidth
+                variant="standard"
+                id="email"
+                name="email"
+                label="Email"
+                autoComplete="off"
+                value={formik.values.email}
+                onChange={formik.handleChange}
+                error={formik.touched.email && Boolean(formik.errors.email)}
+                helperText={formik.touched.email && formik.errors.email}
+              />
+            </Box>
+            <Box sx={{ my: 2 }}>
+              <TextField
+                fullWidth
+                variant="standard"
+                id="password"
+                name="password"
+                label="Password"
+                type="password"
+                value={formik.values.password}
+                onChange={formik.handleChange}
+                error={formik.touched.password && Boolean(formik.errors.password)}
+                helperText={formik.touched.password && formik.errors.password}
+              />
+            </Box>
+            <Box>
+              <Button className="fill-btn" fullWidth sx={{ mt: 2 }} type="submit">
+                Login
+              </Button>
+            </Box>
+            <Box>
+              <span style={{ textAlign: 'center', display: 'block' }}>Don't have an account?</span>
+              <Button className="white-btn" fullWidth sx={{ mt: 2 }} onClick={() => navigate(`/${ROUTES.REGISTER}`)}>
+                Create
+              </Button>
+            </Box>
+          </form>
+        </Box>
+      </Box>
+    </Box>
+  );
 };
 
 export default Login;
--- frontend/src/utils/constants/index.ts
+++ frontend/src/utils/constants/index.ts
@@ -1,3 +1,4 @@
 import { ROUTE_PATHS } from './routePaths';
+import { MESSAGES } from './messages';
 
-export { ROUTE_PATHS as ROUTES };
+export { ROUTE_PATHS as ROUTES, MESSAGES as MSG };
