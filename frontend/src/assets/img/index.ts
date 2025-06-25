a37b48a0c1900659af278138994f5671aed86362
Usman Shaikh <shaikhusman57@gmail.com>
2025-06-25 17:15:50 +0530

    Add styling for home page card images in Home.scss

--- frontend/src/assets/img/index.ts
+++ frontend/src/assets/img/index.ts
@@ -2,12 +2,28 @@ import Logo from './logo.png';
 import Banner from './banner.png';
 import Payment from './payment.png';
 import FooterBg from './footerBg.png';
+import Cat1 from './01-cat.jpg';
+import Cat2 from './02-cat.jpg';
+import Cat3 from './03-cat.jpg';
+import Dog1 from './01-dog.jpg';
+import CatFood from './cat-food.png';
+import CatToy from './cat-toy.jpg';
+import DogFood from './dog-food.png';
+import DogToy from './dog-toy.jpg';
 
 const Images = {
   Logo,
   Banner,
   Payment,
   FooterBg,
+  Cat1,
+  Cat2,
+  Cat3,
+  Dog1,
+  CatFood,
+  CatToy,
+  DogFood,
+  DogToy,
 };
 
 export default Images;
--- frontend/src/pages/Home/Home.tsx
+++ frontend/src/pages/Home/Home.tsx
@@ -1,11 +1,80 @@
-import { Box } from '@mui/material';
+import { Box, Container, Grid, Typography } from '@mui/material';
 import Images from '../../assets/img';
+import './Home.scss';
 
 const Home = () => {
   return (
-    <Box>
+    <Box className="home-page">
       <Box>
         <img src={Images.Banner} className="home-banner" alt="React logo" />
+      </Box>
+      <Box>
+        <Box component="section" py={4}>
+          <Container maxWidth="lg">
+            <Typography variant="h4" gutterBottom>
+              Shop by Pet Type
+            </Typography>
+            <Grid container spacing={2}>
+              <Grid size={{ xs: 12, md: 6 }}>
+                <Box bgcolor="#f5f5f5" p={2} textAlign="center">
+                  <img src={Images.Cat3} className="" alt="Cat And Dog" />
+                </Box>
+              </Grid>
+              <Grid size={{ xs: 12, md: 6 }}>
+                <Box bgcolor="#f5f5f5" p={2} textAlign="center">
+                  <img src={Images.Dog1} className="" alt="Cat And Dog" />
+                </Box>
+              </Grid>
+            </Grid>
+          </Container>
+        </Box>
+
+        <Box component="section" py={4}>
+          <Container maxWidth="lg">
+            <Typography variant="h4" gutterBottom>
+              Top Categories
+            </Typography>
+            <Grid container spacing={2}>
+              <Grid size={{ xs: 12, md: 3 }}>
+                <Box bgcolor="#e8f0fe" p={2} textAlign="center">
+                  <img src={Images.CatFood} className="card-img" alt="Cat And Dog" />
+                </Box>
+              </Grid>
+              <Grid size={{ xs: 12, md: 3 }}>
+                <Box bgcolor="#e8f0fe" p={2} textAlign="center">
+                  <img src={Images.CatToy} className="card-img" alt="Cat And Dog" />
+                </Box>
+              </Grid>
+              <Grid size={{ xs: 12, md: 3 }}>
+                <Box bgcolor="#e8f0fe" p={2} textAlign="center">
+                  <img src={Images.DogFood} className="card-img" alt="Cat And Dog" />
+                </Box>
+              </Grid>
+              <Grid size={{ xs: 12, md: 3 }}>
+                <Box bgcolor="#e8f0fe" p={2} textAlign="center">
+                  <img src={Images.DogToy} className="card-img" alt="Cat And Dog" />
+                </Box>
+              </Grid>
+            </Grid>
+          </Container>
+        </Box>
+
+        <Box component="section" py={4}>
+          <Container maxWidth="lg">
+            <Typography variant="h4" gutterBottom>
+              Best Seller
+            </Typography>
+            <Grid container spacing={2}>
+              {[1, 2, 3, 4].map((item) => (
+                <Grid size={{ xs: 12, md: 3 }} key={item}>
+                  <Box bgcolor="#fff3e0" p={2} textAlign="center">
+                    Best Seller {item}
+                  </Box>
+                </Grid>
+              ))}
+            </Grid>
+          </Container>
+        </Box>
       </Box>
     </Box>
   );
